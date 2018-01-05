b34cff4ce5df9c9cc06271843ebd17bdb19a880f
huehehehe <sowailime@gmail.com>
2018-01-05 17:46:33 +0100

    Działające: szkielet klas, przykładowy scenariusz, zliczanie głębokości danego punktu i długość całkowita scenariusza. Nie działa: dobre wpisywanie scenariusza do struktury.

---
+++ ioprojectarchitecture/ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/logic/Point.java
@@ -0,0 +1,47 @@
+package pl.put.poznan.transformer.logic;
+
+public class Point {
+    private int depth;
+    private int number;
+    private String text;
+    private SubStory subStory;
+
+    public Point(String text, int depth) {
+        this.setText(text);
+        this.setDepth(depth);
+        this.setSubStory(null);
+    }
+
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public SubStory getSubStory() {
+        return subStory;
+    }
+
+    public void setSubStory(SubStory subStory) {
+        this.subStory = subStory;
+    }
+}
